c763421c82352d3f098a48cc94ef805631768f1a
Jurawa Hallen <jurawa@gmail.com>
2016-05-17 00:59:02 -0400

    Layout & Patient view styling with foundation

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "haml-rails", "~> 0.9"
 gem 'foundation-rails'
+gem 'foundation_form_builder'
+gem 'annotate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :integer          not null, primary key
+#  reg_no     :string
+#  birth_year :string
+#  gender     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Patient < ActiveRecord::Base
+  
+  Genders = %w(Male Female)
+  
+  validates :reg_no, :birth_year, :gender, presence: true
+  validates :gender, inclusion: { in: Genders }
+  
 end
