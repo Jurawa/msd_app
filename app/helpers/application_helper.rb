e6ecd92187176101bdf177a4feb3cf59d72ea042
Jurawa Hallen <jurawa@gmail.com>
2016-05-25 10:01:14 -0400

    Cleanup index tables and links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     controller_name.titleize
   end
   
+  def index_links(resource)
+    content_tag :div, class: 'button-group small index-links' do
+      concat link_to 'Show', resource, class: 'button secondary'
+      concat link_to 'Edit', [:edit, resource], class: 'button success'
+      concat link_to 'Destroy', resource, :method => :delete, :data => { :confirm => 'Are you sure?' }, class: 'button alert'
+    end
+  end
+  
 end
