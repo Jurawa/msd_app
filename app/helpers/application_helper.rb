26c3f67e9a0894ee860177a44e4e7d73b1193acc
Jurawa Hallen <jurawa@gmail.com>
2016-06-14 19:35:47 -0700

    Remove option to delete records

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     content_tag :div, class: 'button-group small index-links' do
       concat link_to 'Show', resource, class: 'button secondary'
       concat link_to 'Edit', [:edit, resource], class: 'button success'
-      concat link_to 'Destroy', resource, :method => :delete, :data => { :confirm => 'Are you sure?' }, class: 'button alert'
+      # concat link_to 'Destroy', resource, :method => :delete, :data => { :confirm => 'Are you sure?' }, class: 'button alert'
     end
   end
   
